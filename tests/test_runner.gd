473c9c3e3f1e8c6cc84b341bf5d92f62d7ca2381
jinngimk-lang <jinngimk@gmail.com>
2026-08-13 10:08:12 +0800

    test: run pointer adapter contract suite

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -11,6 +11,7 @@ func _init() -> void:
 	for suite_path in [
 		"res://tests/test_peel_model.gd",
 		"res://tests/test_pointer_state.gd",
+		"res://tests/test_pointer_adapter.gd",
 		"res://tests/test_score_model.gd",
 		"res://tests/test_peel_controller.gd",
 		"res://tests/test_label_visual_geometry.gd"
