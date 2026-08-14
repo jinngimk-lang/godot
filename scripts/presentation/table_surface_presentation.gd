f2854473cd0ac35454ee9de5911d85f71f1cf1ab
jinngimk-lang <jinngimk@gmail.com>
2026-08-14 15:04:42 +0800

    art: smooth the market display counter

--- scripts/presentation/table_surface_presentation.gd
+++ scripts/presentation/table_surface_presentation.gd
@@ -24,19 +24,19 @@ func _process(_delta: float) -> void:
 	if next_id == "night_bar":
 		mat.set_shader_parameter("base_color",Vector3(0.105,0.045,0.018))
 		mat.set_shader_parameter("roughness_value",0.23)
-		mat.set_shader_parameter("grain_strength",0.20)
-		mat.set_shader_parameter("grain_scale",92.0)
+		mat.set_shader_parameter("grain_strength",0.16)
+		mat.set_shader_parameter("grain_scale",72.0)
 		mat.set_shader_parameter("stone_mode",0.0)
 	elif next_id == "market_coldcase":
 		mat.set_shader_parameter("base_color",Vector3(0.72,0.71,0.67))
-		mat.set_shader_parameter("roughness_value",0.36)
-		mat.set_shader_parameter("grain_strength",0.03)
-		mat.set_shader_parameter("grain_scale",42.0)
-		mat.set_shader_parameter("stone_mode",1.0)
+		mat.set_shader_parameter("roughness_value",0.38)
+		mat.set_shader_parameter("grain_strength",0.018)
+		mat.set_shader_parameter("grain_scale",18.0)
+		mat.set_shader_parameter("stone_mode",0.0)
 	else:
 		mat.set_shader_parameter("base_color",Vector3(0.26,0.105,0.035))
 		mat.set_shader_parameter("roughness_value",0.40)
-		mat.set_shader_parameter("grain_strength",0.16)
-		mat.set_shader_parameter("grain_scale",76.0)
+		mat.set_shader_parameter("grain_strength",0.12)
+		mat.set_shader_parameter("grain_scale",58.0)
 		mat.set_shader_parameter("stone_mode",0.0)
 	table.material_override = mat
