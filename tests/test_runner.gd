2c34a16649e73f91d84cc02ddaa439534fe7baf8
jinngimk-lang <jinngimk@gmail.com>
2026-08-18 05:30:55 +0800

    test: run crumple shell contact contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -32,6 +32,7 @@ func _init() -> void:
 		"res://tests/test_ritual_flow.gd",
 		"res://tests/test_cup_crumple_model.gd",
 		"res://tests/test_crumple_hand_staging.gd",
+		"res://tests/test_crumple_shell_contact.gd",
 		"res://tests/test_reference_profiles.gd",
 		"res://tests/test_inspection_controller.gd",
 		"res://tests/test_venue_presentation.gd",
