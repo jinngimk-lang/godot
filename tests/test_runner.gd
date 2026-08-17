137d4a154f072b8258c03e4f891553274c405e46
jinngimk-lang <jinngimk@gmail.com>
2026-08-17 17:48:27 +0800

    test: run tactile peel pacing contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -15,6 +15,7 @@ func _init() -> void:
 		"res://tests/test_score_model.gd",
 		"res://tests/test_peel_controller.gd",
 		"res://tests/test_peel_entry_guidance.gd",
+		"res://tests/test_tactile_peel_pacing.gd",
 		"res://tests/test_label_visual_geometry.gd",
 		"res://tests/test_label_lifecycle.gd",
 		"res://tests/test_label_visual_v2.gd",
