a5bcd03fcf1c3ed216d72b1f499d17b7081bd95a
jinngimk-lang <jinngimk@gmail.com>
2026-08-18 16:30:01 +0800

    feat: tune peel feel per substrate

--- scripts/session/session_model.gd
+++ scripts/session/session_model.gd
@@ -15,6 +15,7 @@ const VARIANTS := [
 		"safe_pull_speed":5.0,
 		"tear_pull_speed":12.0,
 		"residue_gain":0.16,
+		"peel_feel":{"motion_pixels_per_release":3.25,"breakaway_multiplier":1.24,"bend_band_ratio":0.12,"backing_thickness":0.0034},
 		"label_width":0.76,
 		"label_height":0.60,
 		"label_y":0.16,
@@ -33,6 +34,7 @@ const VARIANTS := [
 	{
 		"id":"sauce_jar","name":"Jar","drink":"TOMATO BASIL SAUCE",
 		"base_adhesion":11.6,"release_increment":0.0135,"speed_gain":0.019,"angle_gain":0.32,"bond_response":9.4,"bond_relaxation":3.8,"safe_pull_speed":4.8,"tear_pull_speed":10.4,"residue_gain":0.21,
+		"peel_feel":{"motion_pixels_per_release":4.10,"breakaway_multiplier":1.34,"bend_band_ratio":0.09,"backing_thickness":0.0052},
 		"label_width":0.84,"label_height":0.72,"label_y":-0.01,
 		"label_profile":{"substrate":"rustic_jar_paper","roughness":0.90,"thickness_scale":1.06,"fiber_scale":1.08,"edge_tint":Color(0.72,0.61,0.45),"adhesive_trace":0.22,"adhesive_tint":Color(0.78,0.68,0.52),"fiber_tint":Color(0.90,0.82,0.68),"fiber_gain":1.04},
 		"cup_color":Color(0.88,0.94,0.95),"cup_shell":"glass_jar","cup_dimensions":{"top_radius":0.385,"bottom_radius":0.405,"height":1.28},
@@ -43,6 +45,7 @@ const VARIANTS := [
 	{
 		"id":"tin_can","name":"Tin Can","drink":"GOLDEN PEACHES",
 		"base_adhesion":12.0,"release_increment":0.013,"speed_gain":0.020,"angle_gain":0.29,"bond_response":9.0,"bond_relaxation":3.4,"safe_pull_speed":4.5,"tear_pull_speed":9.5,"residue_gain":0.24,
+		"peel_feel":{"motion_pixels_per_release":3.55,"breakaway_multiplier":1.22,"bend_band_ratio":0.11,"backing_thickness":0.0042},
 		"label_width":0.88,"label_height":0.86,"label_y":-0.04,
 		"label_profile":{"substrate":"grocery_wrap_paper","roughness":0.86,"thickness_scale":1.16,"fiber_scale":1.22,"edge_tint":Color(0.70,0.55,0.34),"adhesive_trace":0.27,"adhesive_tint":Color(0.80,0.68,0.43),"fiber_tint":Color(0.93,0.80,0.58),"fiber_gain":1.18},
 		"cup_color":Color(0.66,0.68,0.70),"cup_shell":"tin","cup_dimensions":{"top_radius":0.405,"bottom_radius":0.398,"height":1.34},
@@ -53,6 +56,7 @@ const VARIANTS := [
 	{
 		"id":"yuzu_bottle","name":"Supermarket","drink":"YUZU SPARKLING",
 		"base_adhesion":13.4,"release_increment":0.015,"speed_gain":0.014,"angle_gain":0.39,"bond_response":9.2,"bond_relaxation":4.8,"safe_pull_speed":5.4,"tear_pull_speed":11.5,"residue_gain":0.19,
+		"peel_feel":{"motion_pixels_per_release":2.75,"breakaway_multiplier":1.12,"bend_band_ratio":0.15,"backing_thickness":0.0028},
 		"label_width":0.92,"label_height":0.62,"label_y":-0.04,
 		"label_profile":{"substrate":"coated_citrus","roughness":0.66,"thickness_scale":0.72,"fiber_scale":0.58,"edge_tint":Color(0.82,0.86,0.72),"adhesive_trace":0.11,"adhesive_tint":Color(0.78,0.86,0.66),"fiber_tint":Color(0.90,0.92,0.82),"fiber_gain":0.65},
 		"cup_color":Color(0.84,0.95,0.94),"cup_shell":"clear_glass","cup_dimensions":{"top_radius":0.335,"bottom_radius":0.315,"height":1.52},
@@ -63,6 +67,7 @@ const VARIANTS := [
 	{
 		"id":"lemon_can","name":"Can","drink":"LEMON SPARKLING SODA",
 		"base_adhesion":12.7,"release_increment":0.014,"speed_gain":0.016,"angle_gain":0.35,"bond_response":9.7,"bond_relaxation":4.1,"safe_pull_speed":5.1,"tear_pull_speed":10.8,"residue_gain":0.18,
+		"peel_feel":{"motion_pixels_per_release":2.40,"breakaway_multiplier":1.08,"bend_band_ratio":0.19,"backing_thickness":0.0022},
 		"label_width":0.90,"label_height":0.88,"label_y":-0.03,
 		"label_profile":{"substrate":"thin_can_wrap","roughness":0.58,"thickness_scale":0.62,"fiber_scale":0.42,"edge_tint":Color(0.88,0.83,0.61),"adhesive_trace":0.14,"adhesive_tint":Color(0.86,0.80,0.55),"fiber_tint":Color(0.95,0.90,0.70),"fiber_gain":0.48},
 		"cup_color":Color(0.74,0.76,0.78),"cup_shell":"aluminum_can","cup_dimensions":{"top_radius":0.385,"bottom_radius":0.402,"height":1.42},
